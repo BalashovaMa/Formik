967d7b3931c351436807c48c1149a5be4a69928e
BalashovaMa <balashova.m.nd@gmail.com>
2023-01-02 16:48:54 +0200

    refactor: form

--- src/Form.js
+++ src/Form.js
@@ -1,24 +1,44 @@
+import { useFormik } from "formik";
+
+const formik = useFormik({
+    initialValues: {
+        name: '',
+        email: '',
+        amount: 0,
+        currency: '',
+        text: '',
+        terms: false
+    },
+    onSubmit: values => console.log(JSON.stringify(values, null, 2))
+})
+
 const Form = () => {
     return (
-        <form className="form">
+        <form className="form" onSubmit={formik.handleSubmit}>
             <h2>Отправить пожертвование</h2>
             <label htmlFor="name">Ваше имя</label>
             <input
                 id="name"
                 name="name"
                 type='text'
+                value={formik.values.name}
+                onChange={formik.handleChange}
             />
             <label htmlFor="email">Ваша почта</label>
             <input
                 id="email"
                 name="email"
                 type='email'
+                value={formik.values.email}
+                onChange={formik.handleChange}
             />
             <label htmlFor="amount">Сумма</label>
             <input
                 id="amount"
                 name="amount"
                 type='number'
+                value={formik.values.amount}
+                onChange={formik.handleChange}
             />
             <label htmlFor="currency">Валюта</label>
             <select name="currency"
